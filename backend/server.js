c6204e6af2efa2762ad34f9cf71938113e66f58e
Victor <victoraltieri1@gmail.com>
2025-09-18 10:15:38 -0300

    Ajustando erros e estilizando

--- backend/server.js
+++ backend/server.js
@@ -1,87 +1,81 @@
-const cors = require('cors') //permite que acesse rotas diferentes (domínios)
-
-const express = require('express') //importa o módulo express que vai construir o servidor
-
-const {v4:uuid} = require("uuid") // função responsável por gerar ID's únicos
-
-const bodyParser = require('body-parser') //middleware que analisa o corpo da requisição de entrada HTTP (dados que vem do formuário)
-
-//Instânciando o express 
+const express = require("express");//importa o módulo express que contruir o servidior
+const cors = require("cors");//permite que acesse rotas diferentes(domínios)
+const bodyParser= require("body-parser")// middleware que analisa o corpo da requisição de entrada HTTP(dados que vem do formulario)
+const {v4:uuid} = require("uuid")//função responsavel por gerar ID´s unicos
+// INSTANCIANDO O EXPRESS
 const app = express();
-
-// define a porta do servidor
+// DEFINE A PORTA DO SERVIDOR
 const Port = 3001;
-
+ 
 //usando o cors
 app.use(cors());
-
-//usando o body parse para a requisição
+// usando o body-parse para a requisição
 app.use(bodyParser.json());
-
-
-//Variavel que recebe um array vazio
-let produtos = [];
-
-//criando a rota cadastrar produto
-app.post("/produto", (req,res)=>{
-    // destruct- requisição das variáveis que serão manipuladas no corpo da aplicação
+ 
+// VARIAVEL QUE RECEBE UM ARRAY VAZIO
+let produtos =[];
+ 
+// CRIANDO A ROTA CADASTRAR PRODUTO (post)
+ 
+app.post("/produto",(req,res)=>{
+    // desestruct- requisição das variaveis que serão manipuladas no corpo da aplicação
     const {nome,descricao} = req.body
-
+    //  validando os campos das variaveis
     if(!nome || !descricao){
-
         return res.status(400).json({error:"Campos inválidos"})
     }
-
-    //realiza o novo cadastro vom id, nome e descrição
-    const novoItem = {id:uuid(),nome,descricao}
-    //pega o que foi cadastrado e coloca no array produtos
+    //  realiza o novo cadastro com id,nome e descricao
+    const novoItem ={id:uuid(),nome,descricao}
+    // pega o que foi cadastraro e coloca no array produtos
     produtos.push(novoItem);
-    res.status(201).json({message:"Cadastro efetuado com sucesso"})
+    // retorna a mensagem de sucesso
+    res.status(201).json({message:"Cadastrado Efetuado com sucesso"})
 })
-
-//Rota para consultar os produtos cadastrados
-app.get('/produto', (req,res)=>{
+ 
+// ROTA PARA CONSULTAR OS PRODUTOS CADASTRADOS (get)
+app.get("/produto",(req,res)=>{
     res.json(produtos)
 })
-
-//rota para alterar produto cadastrado
-app.put("/produto/id:", (req,res)=>{
-    const produtoId = req.params.id; //obtém o id do produto na url
-    const {nome,descrição} = req.body;
-    //validando o campo dos usuários
+ 
+// ROTA PARA ALTERAR PRODUTO CADASTRADO
+ 
+app.put("/produto/:id", (req,res)=>{
+     const produtoId =req.params.id;   //Obtém o ID do produto na url
+     const {nome,descricao}= req.body;
+       //  validando os campos das variaveis
     if(!nome || !descricao){
-
         return res.status(400).json({error:"Campos inválidos"})
     }
-
-    const produtoIndex = produtos.findIndex(item=>item.id === produtoId);
-    if(produtoIndex === -1){
+    const produtoIndex = produtos.findIndex(item=>item.id ===produtoId);
+      if(produtoIndex === -1){
         return res.status(404).json({error:"Produto não encontrado"})
     }
-
-    produtos[produtosIndex] = {id:produtoId, nome, descricao};
+    produtos[produtoIndex]={id:produtoId,nome, descricao};
     res.json(produtos[produtoIndex])
-
+ 
 })
-
-// rota para deletar o produto cadastrado (delete)
-app.delete("/produtos/id:", (req,res)=>{
-    const produtoId = req.params.id; //obtém o id do produto na url
-    // armazena o tamanho inicial do array de produtos
-    const inicioProdutos = produtos.length;
-    produtos = produtos.filter(item => item.id !== produtoId)
-
-    //verifica se o produto foi removido
-    if(produtos.length == inicioProduto){
+ 
+// ROTA PARA DELETAR O PRODUTO CADASTRADO (delete)
+ 
+app.delete("/produto/:id",(req,res)=>{
+     //Obtém o ID do produto na url
+    const produtoId =req.params.id;  
+    // Armazean o tamanho inicial do array de produtos
+    const inicioProduto = produtos.length;
+    // filtra o array, removendo o produto com id escolhido
+    produtos = produtos.filter(item =>item.id !== produtoId)
+ 
+    // verifica se o produto foi removido
+     if(produtos.length == inicioProduto){
         return res.status(404).json({error:"Produto não encontrado"})
     }
-})
-
-  // mensagem afirmando que o produto foi removido
+    // mensagem afirmando que o produto foi removido
     res.status(404).send("Produto removido com sucesso")
-
-
-//localizando a escuta do servidor
+ 
+})
+ 
+ 
+//Executando a escuta do servidor
 app.listen(Port, ()=>{
-    console.log(`Servidor rodando na porta http://localhost:${Port}`)
+    console.log(`Servidor rodando na porta http://locahost:${Port}`)
 })
--- frontend/src/components/Produto.jsx
+++ frontend/src/components/Produto.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from "react";
+	import { useState,useEffect } from "react";
 import axios from 'axios'
 
 const Produto = () => {
@@ -19,7 +19,7 @@ const Produto = () => {
         }
         // TRATAMENTO DE ERROS
         try{
-            const response = await axios.post(API_URL);
+            const response = await axios.post(API_URL,novoProduto);
             setProduto([...produto,response.data])
             setNovoProduto({nome:"", descricao:""})
             setEditar(false);
@@ -38,7 +38,8 @@ const Produto = () => {
     // CONSULTAR PRODUTOS CADASTRADOS
     const consultarProdutos= async ()=>{
         try{
-            const response = await axios.get(API_URL)
+            const response = await axios.get(API_URL);
+            setProduto(response.data);
 
         }
         catch(error){
@@ -68,7 +69,7 @@ const Produto = () => {
 
     // DELETAR UM PRODUTO CADASTRADOS
 
-    const deletarProduto =async ()=>{
+    const deletarProduto =async (id)=>{
         if(window.confirm("Tem certeza que deseja deleta este produto")){
             try{
                 await axios.delete(`${API_URL}/${id}`);
@@ -97,11 +98,11 @@ const Produto = () => {
     }
 
   return (
-    <div>
-      <h1>Cadastro de Produto</h1>
-      <form>
-        <div>
-          <label>Nome Produto</label>
+    <div className="mx-auto p-4">
+      <h1 className=" text-2xl font-bold mb-4">Cadastro de Produto</h1>
+      <form className="mb-4">
+        <div className="mb-4">
+          <label className=" block text-xl font-medium text-gray-700">Nome Produto</label>
           <input 
            type="text"
            id="nome" 
@@ -109,33 +110,38 @@ const Produto = () => {
            value={novoProduto.nome} //pega a variavel do useState
             // pega o que o for digitado no campo    
            onChange={(e)=>setNovoProduto({...novoProduto, nome: e.target.value})}
+           className="mt-2 border rounded w-full"
            
            />
         </div>
 
         <div>
-          <label>Descrição Produto</label>
+          <label className=" block text-xl font-medium text-gray-700">Descrição Produto</label>
           <input
             type="text"
             id="descricao"
             placeholder="Digite descrição Produto"
             value={novoProduto.descricao}
             onChange={(e)=>setNovoProduto({...novoProduto, descricao : e.target.value})}
+            className="mt-2 border rounded w-full"
           />
         </div>
-            <button onClick={handleSubmit}>
+            <button onClick={handleSubmit}
+                className="bg-blue-600 hover:bg-blue-900 tex-white font-bold py-2 px-4 mt-4 rounded">
                 {editar ? "Alterar" : "Cadastrar"}
             </button>
       </form>
       <ul>
         {produto.map(item =>(
-        <li key={item.id}>
+        <li key={item.id} className="border p-2 mb-4 rounded flex items-center justify-between">
             <div>
-                <strong>{item.nome}</strong>{item.descricao}
+                <strong className="font-semibold">{item.nome}</strong>{item.descricao}
             </div>
             <div>
-                <button onClick={handleAlterar(item)}>Editar</button>
-                <button onClick={()=>deletarProduto(item.id)}>Deletar</button>
+                <button onClick={()=>handleAlterar(item)}
+                className="bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-2 px-2 rounded mr-2">Editar</button>
+                <button onClick={()=>deletarProduto(item.id)}
+                     className="bg-red-500 hover:bg-yellow-600 text-black font-bold py-2 px-2 rounded mr-2">Deletar</button>
             </div>
         </li>
         ))}     
